fe1af17eda9e385686cbbdde9ee1cfe3a0c9fdb8
J0YD33P <joyslearninghub@gmail.com>
2024-09-24 00:59:55 +0530

    Improved basic structure and styling and added basic button functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const addBook = document.querySelector(".add-book");
+const bookDetailsContainer = document.querySelector(".book-details-container")
+
+addBook.addEventListener("click", () =>{
+    bookDetailsContainer.style.display = 'flex';
+});
