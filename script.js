b00662d1632abc2b92dac24d0bc4edbdbe97479b
J0YD33P <joyslearninghub@gmail.com>
2024-09-25 01:45:03 +0530

    Working on the functionality of adding a book to the library

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const addBook = document.querySelector(".add-book");
+const addBookButton = document.querySelector(".add-book");
 const bookDetailsContainer = document.querySelector(".book-details-container");
 const bookForm = document.querySelector(".book-details-container");
 
@@ -8,23 +8,36 @@ function Book(title, author, pages, readBook){
     this.author = author;
     this.pages = pages;
     this.readBook = readBook;
-    this.checkRead = function(){
-        if (this.readBook === 'yes'){
-            return "read.";
-        }
-        else{
-            return "not read yet.";
-        }
-
-    }
-    this.info = function(){
-        console.log(`${title} by ${author}, ${pages}, ${this.checkRead()}`);
-    }
-
 }
 
+Book.prototype.checkRead = function(){
+    if (this.readBook === 'yes'){
+        return "read.";
+    }
+    else{
+        return "not read yet.";
+    }
+}
 
-addBook.addEventListener("click", () =>{
+Book.prototype.info = function(){
+    console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.checkRead()}`);
+}
+
+function addBookLibrary(book){
+    const newBookDetails = document.createElement("div");
+    newBookDetails.textContent = book.title;
+    newBookDetails.textContent = book.author;
+    newBookDetails.textContent = book.pages;
+    newBookDetails.textContent = book.readBook;
+    newBookDetails.style.backgroundColor = "rgb(244, 230, 214)";
+    newBookDetails.style.padding = "2em";
+    newBookDetails.style.borderRadius = "10px";
+
+    const bookGrid = document.querySelector(".book-grid");
+    bookGrid.append(newBookDetails);
+}
+
+addBookButton.addEventListener("click", () =>{
     bookDetailsContainer.style.display = 'flex';
 });
 
@@ -45,6 +58,9 @@ bookForm.addEventListener("submit", (event) =>{
         }
     });
 
-    const book1 = new Book(bookTitle, bookAuthor, bookPage, checkSelected);
-    book1.info();
+    let book = new Book(bookTitle, bookAuthor, bookPage, checkSelected);
+    book.info();
+    addBookLibrary(book);
+
+    bookForm.reset();
 });
