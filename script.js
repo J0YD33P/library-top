9f3600c4ddd6ce19ab2012dd4b1fbf6767e8a703
J0YD33P <joyslearninghub@gmail.com>
2024-09-26 02:08:59 +0530

    Added functionality for providing book description along with toggling the read button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const myLibrary = [];
 const addBookButton = document.querySelector(".add-book");
 const bookDetailsContainer = document.querySelector(".book-details-container");
 const bookForm = document.querySelector(".book-details-container");
@@ -23,18 +24,41 @@ Book.prototype.info = function(){
     console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.checkRead()}`);
 }
 
-function addBookLibrary(book){
-    const newBookDetails = document.createElement("div");
-    newBookDetails.textContent = book.title;
-    newBookDetails.textContent = book.author;
-    newBookDetails.textContent = book.pages;
-    newBookDetails.textContent = book.readBook;
-    newBookDetails.style.backgroundColor = "rgb(244, 230, 214)";
-    newBookDetails.style.padding = "2em";
-    newBookDetails.style.borderRadius = "10px";
 
-    const bookGrid = document.querySelector(".book-grid");
-    bookGrid.append(newBookDetails);
+const bookGrid = document.querySelector(".book-grid");
+
+function addBookLibrary(book){
+    const newBookCard = document.createElement("div");
+    newBookCard.classList.add("new-book-card"); // Created for accessing the CSS styling
+    newBookCard.dataset.array = myLibrary.length-1; // Adds custom data-array attribute to each book
+
+    bookGrid.appendChild(newBookCard);
+    
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = `Title: ${book.title}`;
+    
+    const authorElement = document.createElement("h3");
+    authorElement.textContent = `Author: ${book.author}`;
+    
+    const pageElement = document.createElement("h3");
+    pageElement.textContent = `Pages: ${book.pages}`;
+
+    const readButtonElement = document.createElement("button");
+    if(book.readBook === 'yes'){
+        readButtonElement.textContent = 'Read';
+    }
+    else{
+        readButtonElement.textContent = 'Not Read';
+        readButtonElement.classList.add("js-not-read-btn");
+    }
+
+    readButtonElement.classList.add('button', "js-read-btn");
+
+    const removeButtonElement = document.createElement("button");
+    removeButtonElement.textContent = 'Remove';
+    removeButtonElement.classList.add('button', "js-remove-btn");
+
+    newBookCard.append(titleElement, authorElement, pageElement, readButtonElement , removeButtonElement);
 }
 
 addBookButton.addEventListener("click", () =>{
@@ -59,8 +83,26 @@ bookForm.addEventListener("submit", (event) =>{
     });
 
     let book = new Book(bookTitle, bookAuthor, bookPage, checkSelected);
+    myLibrary.push(book);
     book.info();
     addBookLibrary(book);
 
     bookForm.reset();
+});
+
+bookGrid.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("button")){
+        if(event.target.textContent === 'Read'){
+            event.target.textContent = "Not Read";
+            event.target.classList.add("js-not-read-btn");
+        } 
+        else if(event.target.textContent === 'Not Read') {
+            event.target.textContent = "Read";
+            event.target.classList.remove("js-not-read-btn");
+        }
+
+        else{
+            console.log(event.target.parentElement.dataset.array);               
+        }
+    }    
 });
